b55eb49f169a58afca6e07d9df92d214ad88d20c
ayalasegal <ayalasegal1@gmail.com>
2024-01-24 12:19:28 +0200

    Upload Changes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 function findPeakElementRecursive(arr, low, high) {
     if (low === high) {
-      // Only one element in the array, and it is a peak
       return low;
     }
   
     let mid = Math.floor((low + high) / 2);
   
     if (arr[mid] > arr[mid + 1]) {
-      // Peak element is in the left half
       return findPeakElementRecursive(arr, low, mid);
     } else {
-      // Peak element is in the right half
       return findPeakElementRecursive(arr, mid + 1, high);
     }
   }
@@ -19,7 +16,6 @@ function findPeakElementRecursive(arr, low, high) {
     return findPeakElementRecursive(arr, 0, arr.length - 1);
   }
   
-  // Example usage:
   const array = [1, 2, 3, 1];
   const peakIndex = findPeakElement(array);
   const peakElement = array[peakIndex];
